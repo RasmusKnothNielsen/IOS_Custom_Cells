bbb31dcf5ffce06a0182fbbfb43cb35fbfc81ae0
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-03-13 08:34:01 +0100

    Initial Commit

---
+++ CustomCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCell
+//
+//  Created by Rasmus Knoth Nielsen on 13/03/2020.
+//  Copyright © 2020 Rasmus Knoth Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
